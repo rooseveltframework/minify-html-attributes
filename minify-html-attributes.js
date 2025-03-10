424ad338d64138fdc169adfa0fa442c21062dd8b
Eric Newport <kethinov@gmail.com>
2025-03-10 12:30:45 -0400

    1.0.0
    - Initial commit.

---
+++ minify-html-attributes.js
@@ -0,0 +1,326 @@
+const fs = require('fs')
+const path = require('path')
+const postcss = require('postcss')
+const cheerio = require('cheerio')
+const acorn = require('acorn')
+const estraverse = require('estraverse')
+const escodegen = require('escodegen')
+
+function loopThroughFilesSync (dir) {
+  let fileList = []
+  let files
+  try {
+    files = fs.readdirSync(dir, { withFileTypes: true })
+  } catch (err) {
+    return fileList
+  }
+  files.forEach(file => {
+    const filePath = path.join(dir, file.name)
+    if (file.isDirectory()) fileList = fileList.concat(loopThroughFilesSync(filePath)) // recurse dirs
+    else if (file.isFile() && !file.name.startsWith('.')) fileList.push(filePath) // exclude hidden files
+  })
+  return fileList
+}
+
+function isBinaryFile (filePath, bytesToCheck = 512) {
+  const buffer = Buffer.alloc(bytesToCheck)
+  const fd = fs.openSync(filePath, 'r')
+  const bytesRead = fs.readSync(fd, buffer, 0, bytesToCheck, 0)
+  fs.closeSync(fd)
+  for (let i = 0; i < bytesRead; i++) {
+    const byte = buffer[i]
+    if (byte === 0) return true // null byte found, likely a binary file
+    else if ((byte < 32 || byte > 126) && byte !== 10 && byte !== 13 && byte !== 9) return true // non-printable ascii character found, likely a binary file; allow common control characters: \n (10), \r (13), \t (9)
+  }
+  return false
+}
+
+function toCamelCase (str) {
+  return str.replace(/-([a-z])/g, (match, letter) => letter.toUpperCase())
+}
+
+// generator function to produce a sequence of unique, minified names sequentially
+function * generateMinifiedNames () {
+  const chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+  let length = 1
+  let index = 0
+  const usedNames = new Set()
+
+  while (true) {
+    const maxIndex = Math.pow(chars.length, length)
+    if (index >= maxIndex) {
+      index = 0
+      length++
+    }
+
+    let name = ''
+    let temp = index
+    for (let j = 0; j < length; j++) {
+      name = chars[temp % chars.length] + name
+      temp = Math.floor(temp / chars.length)
+    }
+
+    if (!usedNames.has(name)) {
+      usedNames.add(name)
+      yield name
+    }
+
+    index++
+  }
+}
+
+function minifyHtmlAttributes (params) {
+  const minifiedNameGenerator = generateMinifiedNames() // create an instance of the name generator
+  const nameMap = {} // store the mapping of original -> minified names
+  let attrsToRename = ['class', 'id', 'name'] // default attributes to rename (aside from data-* attrs)
+  if (params?.extraAttributes) for (const attr of params?.extraAttributes) attrsToRename.push(attr) // allow user to set additional ones
+  if (params?.disableClassReplacements) attrsToRename = attrsToRename.filter(item => item !== 'class')
+  if (params?.disableIdReplacements) attrsToRename = attrsToRename.filter(item => item !== 'id')
+  if (params?.disableNameReplacements) attrsToRename = attrsToRename.filter(item => item !== 'name')
+
+  // replaces original names with their new names
+  function replaceNames (content) {
+    for (const [original, minified] of Object.entries(nameMap)) {
+      const regex = new RegExp(`\\b${original}\\b`, 'g')
+      content = content.replace(regex, minified)
+    }
+    return content
+  }
+
+  function processHtmlContent (html) {
+    const $ = cheerio.load(html, { xml: { xmlMode: false, lowerCaseAttributeNames: false, decodeEntities: false } })
+    $('*').each((i, el) => {
+      const attributes = $(el).attr()
+      for (const attr in attributes) {
+        if (attrsToRename.includes(attr) || (attr.startsWith('data-') && !params?.disableDataReplacements)) {
+          const values = attributes[attr].split(' ')
+          const newValues = values.map(value => {
+            if (!nameMap[value]) {
+              const minified = minifiedNameGenerator.next().value
+              nameMap[value] = minified
+            }
+            return nameMap[value]
+          })
+          $(el).attr(attr, newValues.join(' '))
+        }
+      }
+    })
+
+    // process inline css
+    $('style').each((i, el) => {
+      const css = $(el).html()
+      const result = processCssContent(css)
+      $(el).html(result)
+    })
+
+    // process inline js
+    $('script').each((i, el) => {
+      const js = $(el).html()
+      const result = processJsContent(js)
+      $(el).html(result)
+    })
+
+    // update ids for any element with these attributes to match the new names
+    const attributesToUpdate = ['aria-activedescendant', 'aria-controls', 'aria-describedby', 'aria-labelledby', 'aria-owns', 'for', 'form', 'headers', 'itemref', 'list', 'usemap']
+    $('*').each((i, el) => {
+      const attributes = $(el).attr()
+      for (const attr of attributesToUpdate) {
+        if (attributes[attr]) {
+          const values = attributes[attr].split(' ')
+          const newValues = values.map(value => nameMap[value] || value)
+          $(el).attr(attr, newValues.join(' '))
+        }
+      }
+    })
+
+    // process event handler attributes as inline js
+    const eventHandlerAttributesToUpdate = ['onabort', 'oncanplay', 'oncanplaythrough', 'onchange', 'onclick', 'oncontextmenu', 'oncopy', 'oncut', 'ondblclick', 'ondrag', 'ondragend', 'ondragenter', 'ondragexit', 'ondragleave', 'ondragover', 'ondragstart', 'ondrop', 'ondurationchange', 'onemptied', 'onended', 'onerror', 'onfocus', 'oninput', 'oninvalid', 'onkeydown', 'onkeypress', 'onkeyup', 'onload', 'onloadeddata', 'onloadedmetadata', 'onloadstart', 'onmousedown', 'onmouseenter', 'onmouseleave', 'onmousemove', 'onmouseout', 'onmouseover', 'onmouseup', 'onpaste', 'onpause', 'onplay', 'onplaying', 'onprogress', 'onratechange', 'onreset', 'onscroll', 'onseeked', 'onseeking', 'onselect', 'onshow', 'onsort', 'onstalled', 'onsubmit', 'onsuspend', 'ontimeupdate', 'ontoggle', 'onunload', 'onvolumechange', 'onwaiting', 'onwheel']
+    $('*').each((i, el) => {
+      const attributes = $(el).attr()
+      for (const attr of eventHandlerAttributesToUpdate) {
+        if (attributes[attr]) {
+          const js = attributes[attr]
+          const result = processJsContent(js)
+          $(el).attr(attr, result)
+        }
+      }
+    })
+
+    return $.html()
+  }
+
+  function processCssContent (css) {
+    const root = postcss.parse(css)
+
+    root.walkRules(rule => {
+      rule.selectors = rule.selectors.map(selector => {
+        return selector.replace(/\.([a-zA-Z0-9_-]+)/g, (match, className) => {
+          if (!nameMap[className]) {
+            const minified = minifiedNameGenerator.next().value
+            nameMap[className] = minified
+          }
+          return `.${nameMap[className]}`
+        }).replace(/#([a-zA-Z0-9_-]+)/g, (match, id) => {
+          if (!nameMap[id]) {
+            const minified = minifiedNameGenerator.next().value
+            nameMap[id] = minified
+          }
+          return `#${nameMap[id]}`
+        })
+      })
+    })
+
+    root.walkDecls(decl => {
+      if (decl.value.includes('attr(')) {
+        decl.value = replaceNames(decl.value)
+      }
+      if (decl.prop.startsWith('--')) {
+        decl.value = replaceNames(decl.value)
+      }
+    })
+
+    return root.toString()
+  }
+
+  function processJsContent (js) {
+    const ast = acorn.parse(js, { ecmaVersion: 2020 })
+
+    estraverse.replace(ast, {
+      enter (node) {
+        if (node.type === 'Literal' && typeof node.value === 'string') {
+          // check if the string contains html or css
+          if (node.value.trim().startsWith('<') && node.value.trim().endsWith('>')) {
+            node.value = processHtmlContent(node.value)
+          } else if (node.value.trim().includes('{') && node.value.trim().includes('}')) {
+            node.value = processCssContent(node.value)
+          } else {
+            node.value = replaceNames(node.value)
+          }
+        }
+        if (node.type === 'TemplateLiteral') {
+          node.quasis.forEach(quasi => {
+            if (quasi.value.raw.trim().startsWith('<') && quasi.value.raw.trim().endsWith('>')) {
+              quasi.value.raw = processHtmlContent(quasi.value.raw)
+              quasi.value.cooked = processHtmlContent(quasi.value.cooked)
+            } else if (quasi.value.raw.trim().includes('{') && quasi.value.raw.trim().includes('}')) {
+              quasi.value.raw = processCssContent(quasi.value.raw)
+              quasi.value.cooked = processCssContent(quasi.value.cooked)
+            } else {
+              quasi.value.raw = replaceNames(quasi.value.raw)
+              quasi.value.cooked = replaceNames(quasi.value.cooked)
+            }
+          })
+        }
+        if (node.type === 'BinaryExpression' && node.operator === '+') {
+          if (node.left.type === 'Literal' && typeof node.left.value === 'string') {
+            node.left.value = replaceNames(node.left.value)
+          }
+          if (node.right.type === 'Literal' && typeof node.right.value === 'string') {
+            node.right.value = replaceNames(node.right.value)
+          }
+        }
+        if (node.type === 'Identifier') {
+          const identifierName = node.name
+          if (nameMap[identifierName]) {
+            node.name = nameMap[identifierName]
+          }
+        }
+        if (node.type === 'MemberExpression' && node.property.type === 'Identifier') {
+          const propertyName = node.property.name
+          if (nameMap[propertyName]) {
+            node.property.name = nameMap[propertyName]
+          }
+        }
+        if (node.type === 'MemberExpression' && node.object.type === 'MemberExpression' && node.object.property.name === 'dataset') {
+          const dataAttrName = node.property.name
+          const originalAttrName = `data-${dataAttrName.replace(/([A-Z])/g, '-$1').toLowerCase()}`
+          if (nameMap[originalAttrName]) {
+            node.property.name = nameMap[originalAttrName]
+          }
+        }
+        if (node.type === 'CallExpression' && node.callee.type === 'MemberExpression' && node.callee.property.name === 'querySelector') {
+          const arg = node.arguments[0]
+          if (arg && arg.type === 'Literal' && arg.value.includes('[') && arg.value.includes(']')) {
+            const attrName = arg.value.match(/\[([^\]]+)\]/)[1]
+            if (nameMap[attrName]) {
+              const minifiedAttrName = nameMap[attrName]
+              arg.value = arg.value.replace(attrName, minifiedAttrName)
+              const camelCaseAttrName = toCamelCase(minifiedAttrName.replace(/^data-/, ''))
+              if (node.parent && node.parent.type === 'MemberExpression' && node.parent.property.type === 'Identifier' && node.parent.property.name === attrName) {
+                node.parent.property.name = camelCaseAttrName
+              }
+            }
+          }
+        }
+      }
+    })
+
+    return escodegen.generate(ast)
+  }
+
+  const htmlFiles = []
+  const cssFiles = []
+  const jsFiles = []
+  const editedFiles = {}
+
+  // gather list of potential html files to edit
+  const potentialHtmlFiles = loopThroughFilesSync(params?.htmlDir)
+  for (const file of potentialHtmlFiles) {
+    if (!isBinaryFile(file)) {
+      htmlFiles.push(file)
+    }
+  }
+
+  // gather list of potential css files to edit
+  const potentialCssFiles = loopThroughFilesSync(params?.cssDir)
+  for (const file of potentialCssFiles) {
+    if (!isBinaryFile(file) && file?.endsWith('.css')) {
+      cssFiles.push(file)
+    }
+  }
+
+  // gather list of potential js files to edit
+  const potentialJsFiles = loopThroughFilesSync(params?.jsDir)
+  for (const file of potentialJsFiles) {
+    if (!isBinaryFile(file) && file?.endsWith('.js')) {
+      jsFiles.push(file)
+    }
+  }
+
+  // process html files first to gather class, ID, and data-* mappings
+  for (const file of htmlFiles) {
+    const editedFile = processHtmlContent(fs.readFileSync(file, 'utf8'))
+    if (editedFile) {
+      editedFiles[file] = {
+        type: 'html',
+        contents: editedFile
+      }
+    }
+  }
+
+  // process CSS files using the gathered mappings
+  for (const file of cssFiles) {
+    const editedFile = processCssContent(fs.readFileSync(file, 'utf8'))
+    if (editedFile) {
+      editedFiles[file] = {
+        type: 'css',
+        contents: editedFile
+      }
+    }
+  }
+
+  // process js files using the gathered mappings
+  for (const file of jsFiles) {
+    const editedFile = processJsContent(fs.readFileSync(file, 'utf8'))
+    if (editedFile) {
+      editedFiles[file] = {
+        type: 'js',
+        contents: editedFile
+      }
+    }
+  }
+
+  return editedFiles
+}
+
+module.exports = minifyHtmlAttributes
---
+++ test/test.js
@@ -0,0 +1,193 @@
+/* eslint-env mocha */
+const assert = require('assert')
+const { exec } = require('child_process')
+const fs = require('fs')
+const path = require('path')
+const { chromium } = require('playwright')
+const sampleAppDir = path.join(__dirname, '..', 'sampleApps', 'express')
+
+before(async function () {
+  await new Promise((resolve, reject) => {
+    const serverProcess = exec('npm start', { cwd: sampleAppDir })
+    serverProcess.stdout.on('data', (data) => {
+      if (data.toString().includes('express sample app server is running')) resolve()
+    })
+    serverProcess.stderr.on('data', (data) => {
+      console.error(data.toString())
+      console.error('Make sure to `npm ci` in the sampleApp/express directory first!')
+    })
+    serverProcess.on('error', (error) => reject(error))
+    this.serverProcess = serverProcess
+  })
+})
+
+after(async function () {
+  if (this.serverProcess) this.serverProcess.kill()
+})
+
+describe('minify-html-attributes tests', function () {
+  it('should replace IDs', async function () {
+    const filePath = path.join(sampleAppDir, 'mvc', '.preprocessed_views', 'pageWithForm.html')
+    const fileContent = fs.readFileSync(filePath, 'utf8')
+    assert(!fileContent.includes('for="your_name"'), 'Expected markup not found')
+  })
+
+  it('should have first <p> tag on main page be red', async function () {
+    const browser = await chromium.launch()
+    const page = await browser.newPage()
+    await page.goto('http://localhost:3000/')
+    const firstPColor = await page.evaluate(() => {
+      const firstP = document.querySelector('p:first-of-type')
+      return window.getComputedStyle(firstP).color
+    })
+    assert.strictEqual(firstPColor, 'rgb(255, 0, 0)')
+    await browser.close()
+  })
+
+  it('should have second <p> tag on main page be purple', async function () {
+    const browser = await chromium.launch()
+    const page = await browser.newPage()
+    await page.goto('http://localhost:3000/')
+    const secondPColor = await page.evaluate(() => {
+      const secondP = document.querySelector('p:nth-of-type(2)')
+      return window.getComputedStyle(secondP).color
+    })
+    assert.strictEqual(secondPColor, 'rgb(255, 0, 255)')
+    await browser.close()
+  })
+
+  it('should have first <p> tag on main page be green and underlined', async function () {
+    const browser = await chromium.launch()
+    const page = await browser.newPage()
+    await page.goto('http://localhost:3000/')
+    const firstPStyle = await page.evaluate(() => {
+      const thirdP = document.querySelector('p:nth-of-type(3)')
+      const color = window.getComputedStyle(thirdP).color
+      const textDecoration = window.getComputedStyle(thirdP).textDecoration
+      return { color, textDecoration }
+    })
+    assert.strictEqual(firstPStyle.color, 'rgb(0, 255, 0)')
+    assert(firstPStyle.textDecoration.includes('underline'))
+    await browser.close()
+  })
+
+  it('should successfully update an ID query selector in the JS', async function () {
+    const browser = await chromium.launch()
+    const page = await browser.newPage()
+    let consoleLogFound = false
+    page.on('console', msg => {
+      if (msg.text().includes('id query selector')) consoleLogFound = true
+    })
+    await page.goto('http://localhost:3000/')
+    assert(consoleLogFound, 'Expected console log not found')
+    await browser.close()
+  })
+
+  it('should successfully update a class query selector in the JS', async function () {
+    const browser = await chromium.launch()
+    const page = await browser.newPage()
+    let consoleLogFound = false
+    page.on('console', msg => {
+      if (msg.text().includes('class query selector')) consoleLogFound = true
+    })
+    await page.goto('http://localhost:3000/')
+    assert(consoleLogFound, 'Expected console log not found')
+    await browser.close()
+  })
+
+  it('should successfully update a data attr query selector in the JS', async function () {
+    const browser = await chromium.launch()
+    const page = await browser.newPage()
+    let consoleLogFound = false
+    page.on('console', msg => {
+      if (msg.text().includes('data query selector')) consoleLogFound = true
+    })
+    await page.goto('http://localhost:3000/')
+    assert(consoleLogFound, 'Expected console log not found')
+    await browser.close()
+  })
+
+  it('should successfully update template literal in the JS', async function () {
+    const browser = await chromium.launch()
+    const page = await browser.newPage()
+    let consoleLogFound = false
+    page.on('console', msg => {
+      if (msg.text().includes('template literal')) consoleLogFound = true
+    })
+    await page.goto('http://localhost:3000/')
+    assert(consoleLogFound, 'Expected console log not found')
+    await browser.close()
+  })
+
+  it('should successfully update variable concatenation in the JS', async function () {
+    const browser = await chromium.launch()
+    const page = await browser.newPage()
+    let consoleLogFound = false
+    page.on('console', msg => {
+      if (msg.text().includes('concat')) consoleLogFound = true
+    })
+    await page.goto('http://localhost:3000/')
+    assert(consoleLogFound, 'Expected console log not found')
+    await browser.close()
+  })
+
+  it('should successfully update element.dataset in the JS', async function () {
+    const browser = await chromium.launch()
+    const page = await browser.newPage()
+    let consoleLogFound = false
+    page.on('console', msg => {
+      if (msg.text().includes('dataset 1')) consoleLogFound = true
+    })
+    await page.goto('http://localhost:3000/')
+    assert(consoleLogFound, 'Expected console log not found')
+    await browser.close()
+  })
+
+  it('should successfully update element.dataset with camelCase in the JS', async function () {
+    const browser = await chromium.launch()
+    const page = await browser.newPage()
+    let consoleLogFound = false
+    page.on('console', msg => {
+      if (msg.text().includes('dataset 2')) consoleLogFound = true
+    })
+    await page.goto('http://localhost:3000/')
+    assert(consoleLogFound, 'Expected console log not found')
+    await browser.close()
+  })
+
+  it('should successfully update dynamic variable construction in the JS', async function () {
+    const browser = await chromium.launch()
+    const page = await browser.newPage()
+    let consoleLogFound = false
+    page.on('console', msg => {
+      if (msg.text().includes('dynamic construction 1')) consoleLogFound = true
+    })
+    await page.goto('http://localhost:3000/')
+    assert(consoleLogFound, 'Expected console log not found')
+    await browser.close()
+  })
+
+  it('should successfully update document.forms in the JS', async function () {
+    const browser = await chromium.launch()
+    const page = await browser.newPage()
+    let consoleLogFound = false
+    page.on('console', msg => {
+      if (msg.text().includes('document.forms test')) consoleLogFound = true
+    })
+    await page.goto('http://localhost:3000/pageWithForm')
+    assert(consoleLogFound, 'Expected console log not found')
+    await browser.close()
+  })
+
+  it('should successfully update global variables derived from input IDs in the JS', async function () {
+    const browser = await chromium.launch()
+    const page = await browser.newPage()
+    let consoleLogFound = false
+    page.on('console', msg => {
+      if (msg.text().includes('global var test')) consoleLogFound = true
+    })
+    await page.goto('http://localhost:3000/pageWithForm')
+    assert(consoleLogFound, 'Expected console log not found')
+    await browser.close()
+  })
+})
